4cf4ab711734d249ce18cd7c0c9661d43c42abc9
elenaburdin <66010914+elenaburdin@users.noreply.github.com>
2023-01-23 18:19:58 +0100

    Add files via upload

---
+++ selectors.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('.done');
+const doneTodos = document.querySelectorAll('.done');
+console.dir(doneTodos);
+
+document.querySelector('input[type="checkbox"]');
+const checkbox = document.querySelector('input[type="checkbox"]');
+console.dir(checkbox)
